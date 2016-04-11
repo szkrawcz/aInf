b078a33beec59787186ff1af58d38d0189352738
Szymon <szkrawcz@gmail.com>
2016-04-11 12:52:21 +0200

    created dirgrinder.h and dirgrinder.cpp moved functions to files above creaed function FileHash - calculate file md5

---
+++ dirgrinder.h
@@ -0,0 +1,4 @@
+void ListFilesInDirectory(QDir dir);
+void ListDirectory(QDir dir, bool Recurse);
+void ListContentOfDirectory(QDir dir, bool Recurse);
+void FileHash(QString FileName );
--- main.cpp
+++ main.cpp
@@ -2,90 +2,14 @@
 #include <QDir>
 #include <QCryptographicHash>
 #include <iostream>
+#include "dirgrinder.h"
 
 void ListFilesInDirectory(QDir dir);
 void ListDirectory(QDir dir, bool Recurse);
 
 
-void ListContentOfDirectory(QDir dir, bool Recurse)
-{
-    //ListFilesInDirectory(dir);
-    dir.setFilter(  QDir::Hidden | QDir::NoSymLinks |QDir::AllDirs );
-    dir.setSorting(QDir::Size | QDir::Reversed);
-    QFileInfoList list = dir.entryInfoList();
-    for (int i = 0; i < list.size(); ++i)
-    {
-
-       QFileInfo fileInfo = list.at(i);
-       QString str1 = fileInfo.fileName();
-       QString str2 = ".";
-       QString str3 = "..";
-       if (!str1.compare(str2) || !str1.compare(str3) )
-       {
-           // std::cout <<  " found . or .. ";
-       }
-       else
-       {
-
-       std::cout << qPrintable(QString("%1").arg(fileInfo.absoluteFilePath()));
-       std::cout << std::endl;
-       ListFilesInDirectory(fileInfo.absoluteFilePath());
-       QDir NextDir(fileInfo.absoluteFilePath());
-       if (Recurse) ListContentOfDirectory(NextDir,Recurse);
-       }
-    }
-
-}
-
-void ListFilesInDirectory(QDir dir)
-{
-       dir.setFilter(QDir::Files | QDir::Hidden | QDir::NoSymLinks);
-       dir.setSorting(QDir::Size | QDir::Reversed);
-       QFileInfoList list = dir.entryInfoList();
-       for (int i = 0; i < list.size(); ++i)
-       {
-          QFileInfo fileInfo = list.at(i);
-          QString str1 = fileInfo.fileName();
-          QString str2 = ".";
-          QString str3 = "..";
-          if (!str1.compare(str2) || !str1.compare(str3) )
-          {
-              // std::cout <<  " found . or .. ";
-          }
-          else
-          {
-          std::cout << qPrintable(QString("%1").arg(fileInfo.absoluteFilePath()));
-          std::cout << std::endl;
-          }
-       }
-}
-
-void ListDirectory(QDir dir, bool Recurse)
-{
-    dir.setFilter(  QDir::Hidden | QDir::NoSymLinks |QDir::AllDirs );
-    dir.setSorting(QDir::Size | QDir::Reversed);
-    QFileInfoList list = dir.entryInfoList();
-    for (int i = 0; i < list.size(); ++i)
-    {
-       QFileInfo fileInfo = list.at(i);
-       QString str1 = fileInfo.fileName();
-       QString str2 = ".";
-       QString str3 = "..";
-       if (!str1.compare(str2) || !str1.compare(str3) )
-       {
-           // std::cout <<  " found . or .. ";
-       }
-       else
-       {
-       std::cout << qPrintable(QString("%1").arg(fileInfo.absoluteFilePath()));
-       std::cout << std::endl;
-       QDir NextDir(fileInfo.absoluteFilePath());
-       if (Recurse) ListDirectory(NextDir,1);
-       }
-    }
 
 
-}
 
 int main(int argc, char *argv[])
 {
@@ -95,16 +19,11 @@ int main(int argc, char *argv[])
    //ListFilesInDirectory(dir);
    //---ListFilesInDirectory(dir);
    //---ListContentOfDirectory(dir, 1);
+  //  QFile plik("/home/szymon/c/main");
+     QString FileName = "/home/szymon/c/main";
+     FileHash(FileName);
 
 
-   QCryptographicHash crypto(QCryptographicHash::Sha1);
-   QFile file("F:/ps2/Bios/scph10000.bin");
-   file.open(QFile::ReadOnly);
-   while(!file.atEnd()){
-     crypto.addData(file.read(8192));
-   }
-   QByteArray hash = crypto.result();
-   std::cout<< QString(hash.toHex()).toStdString() <<std::endl;
    return 0;
    //return a.exec();
 }
